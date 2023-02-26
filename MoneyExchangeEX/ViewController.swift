c3e853c30433bdde81c22ff2e120d74da0d69082
hakulee <leesh762@gmail.com>
2023-02-26 20:42:14 +0900

    ADD: 경고창 추가

--- MoneyExchangeEX/ViewController.swift
+++ MoneyExchangeEX/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var wonTextField: UITextField!
     @IBOutlet weak var dollarTextField: UITextField!
     
-    // 10의 3제곱
+    // 10의 3제곱, 소수점 자리수 3개로 제한한
     let squareOfTen: Double = pow(10, 3)
+    
+    // 숫자 외의 값이 들어왔을 때 경고창 띄우는 함수
+    func warningMessage() {
+        let alert = UIAlertController(title: "환산 불가", message: "숫자만 입력해 주세요", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(confirm)
+        present(alert, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +33,8 @@ class ViewController: UIViewController {
         if let won = Double(wonTextField.text!), let exchangeRate = Double(exchangeRateTextField.text!) {
             let result = won / exchangeRate
             dollarTextField.text = "\(round(result * squareOfTen) / squareOfTen)"
+        } else {
+            warningMessage()
         }
     }
     
@@ -33,6 +43,8 @@ class ViewController: UIViewController {
         if let won = Double(wonTextField.text!), let exchangeRate = Double(exchangeRateTextField.text!) {
             let result = exchangeRate / won
             wonTextField.text = "\(round(result * squareOfTen) / squareOfTen)"
+        } else {
+            warningMessage()
         }
     }
 }
