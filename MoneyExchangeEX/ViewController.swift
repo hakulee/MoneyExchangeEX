2eb79d6b08836e1702adf19f69cd62d90fb0259c
hakulee <leesh762@gmail.com>
2023-02-27 14:13:17 +0900

    ADD: 뷰 슈정 및 코드 수정

--- MoneyExchangeEX/ViewController.swift
+++ MoneyExchangeEX/ViewController.swift
@@ -16,16 +16,16 @@ class ViewController: UIViewController {
     // 10의 3제곱, 소수점 자리수 3개로 제한한
     let squareOfTen: Double = pow(10, 3)
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     // 숫자 외의 값이 들어왔을 때 경고창 띄우는 함수
     func warningMessage() {
         let alert = UIAlertController(title: "환산 불가", message: "숫자만 입력해 주세요", preferredStyle: .alert)
         let confirm = UIAlertAction(title: "확인", style: .default)
         alert.addAction(confirm)
         present(alert, animated: true)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
     
     // won에서 dollar로 변환 버튼
@@ -40,8 +40,8 @@ class ViewController: UIViewController {
     
     // dollar에서 won으로 변환 버튼
     @IBAction func dollarToWonButton(_ sender: Any) {
-        if let won = Double(wonTextField.text!), let exchangeRate = Double(exchangeRateTextField.text!) {
-            let result = exchangeRate / won
+        if let dollar = Double(dollarTextField.text!), let exchangeRate = Double(exchangeRateTextField.text!) {
+            let result = exchangeRate * dollar
             wonTextField.text = "\(round(result * squareOfTen) / squareOfTen)"
         } else {
             warningMessage()
